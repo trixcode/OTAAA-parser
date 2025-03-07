1e1e599af033f4e36def775021a9bfe817628e22
Kirill-Dr <daright6@gmail.com>
2025-03-07 15:02:32 +0600

    feat: added tasks

--- app/config.py
+++ app/config.py
@@ -1,9 +1,8 @@
 from celery import Celery
 
 celery_app = Celery('app')
+celery_app.config_from_object('app.config')
 
 celery_app.conf.broker_url = 'redis://redis:6379/0'
 celery_app.conf.result_backend = 'redis://redis:6379/0'
 celery_app.conf.timezone = 'UTC'
-
-celery_app.config_from_object('app.config')
--- app/worker/tasks.py
+++ app/worker/tasks.py
@@ -1,33 +1,34 @@
-from celery import Celery
+from app.config import celery_app
 from celery.schedules import crontab
 from app.parsers.fomenki_parser_main import get_main_data
 from app.parsers.fomenki_parser_detail import fomenki_parser_detail
 from app.parsers.fomenki_parser_critical_afishas import fomenki_parser_critical_afishas
 from celery import chain
 
-celery_app = Celery('app')
-celery_app.config_from_object('app.config')
-
 @celery_app.task
 def scheduled_parsing_main(*args, **kwargs):
     get_main_data()
 
-# @celery_app.task
-# def scheduled_parsing_detail(*args, **kwargs):
-#     fomenki_parser_detail()
-#
-# @celery_app.task
-# def scheduled_parsing_critical(*args, **kwargs):
-#     fomenki_parser_critical_afishas()
-#
-# @celery_app.task
-# def scheduled_parsing(*args, **kwargs):
-#     task_chain = chain(scheduled_parsing_main.s(), scheduled_parsing_detail.s(), scheduled_parsing_critical.s())
-#     task_chain()
+@celery_app.task
+def scheduled_parsing_detail(*args, **kwargs):
+    fomenki_parser_detail()
+
+@celery_app.task
+def scheduled_parsing_critical(*args, **kwargs):
+    fomenki_parser_critical_afishas()
+
+@celery_app.task
+def scheduled_parsing(*args, **kwargs):
+    task_chain = chain(
+        scheduled_parsing_main.s(),
+        scheduled_parsing_detail.s(),
+        scheduled_parsing_critical.s()
+    )
+    task_chain()
 
 celery_app.conf.beat_schedule = {
     "run-update-spectacles": {
-        "task": "app.worker.tasks.scheduled_parsing_main",
-        "schedule": crontab(minute=0, second=30),
+        "task": "app.worker.tasks.scheduled_parsing",
+        "schedule": crontab(hour=20, minute=0),
     }
 }
